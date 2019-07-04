0b2a40a4899bf35ac96d50d87770b832ccb717ce
Dr. Németh Tamás <tomuwhu@gmail.com>
2019-07-04 15:17:19 +0200

    .

--- index.js
+++ index.js
@@ -2,26 +2,29 @@ var SSE  = require('sse'),
     http = require('http'),
     fs   = require('fs'),
     clients = [],
-    counter = 0,
+    echo = '',
     frontend;
 
 fs.readFile( './frontend.html', (err,s) => {
   frontend = s.toString() ;
 });
 
-setInterval( () => {
-  clients.forEach( v => v.send('X-'+counter++) );
-}, 5000);
-
 var server = http.createServer( (req, res) => {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(frontend);
+  if (req.url==='/')
+    res.end(frontend);
+  else if (req.url==='/favicon.ico')
+    res.end(null);
+  else {
+    echo=req.url.slice(1);
+    clients.forEach( v => v.send( echo ) );
+    res.end(null);
+  }
 });
  
 server.listen(8080, 'localhost', () => {
   var sse = new SSE(server);
   sse.on('connection', (client) => {
     clients.push(client);
-    client.send('hi there!');
   });
 });
