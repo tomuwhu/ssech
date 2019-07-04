45dafc4240a39eaea19a674650706ff4cef7c508
Dr. Németh Tamás <tomuwhu@gmail.com>
2019-07-04 16:32:08 +0200

    .

--- index.js
+++ index.js
@@ -4,11 +4,9 @@ var SSE  = require('sse'),
     clients = [],
     echo = '',
     frontend;
-
 fs.readFile( './frontend.html', (err,s) => {
   frontend = s.toString() ;
 });
-
 var server = http.createServer( (req, res) => {
   res.writeHead(200, {'Content-Type': 'text/html'});
   if (req.url==='/')
