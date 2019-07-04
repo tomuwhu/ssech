d332249b328679d220ab03008caec2cf669e606a
Dr. Németh Tamás <tomuwhu@gmail.com>
2019-07-04 20:58:15 +0200

    .

--- index.js
+++ index.js
@@ -4,18 +4,19 @@ var SSE  = require('sse'),
     ct   = [],
 //  myip = '192.168.1.103', //set your public ip
     myip = 'localhost',     //or localhost to test in locale
-    port = 3000 ;           //set your port number (http://192.168.1.103:8080/)
+    port = 3004,            //set your port number (http://192.168.1.103:8080/)
+    base = '/' ;
 
 rf  .setenv('frontend');
 
 var server = http.createServer( (req, res) => {
   res.writeHead(200, {'Content-Type': 'text/html'});
   switch (req.url) {
-    case '/': res.end(rf.file()); break;
-    case '/vue.js': res.end(rf.vuejs); break;
-    case '/axios.min.js': res.end(rf.axiosjs); break;
-    case '/axios.min.map': res.end(null); break;
-    case '/favicon.ico': res.end(null); break;
+    case base: res.end(rf.file()); break;
+    case base+'vue.js': res.end(rf.vuejs); break;
+    case base+'axios.min.js': res.end(rf.axiosjs); break;
+    case base+'axios.min.map': res.end(null); break;
+    case base+'favicon.ico': res.end(null); break;
     default: {
         ct.forEach( v => v.send( req.url.slice(1) ) );
         res.end(null);
