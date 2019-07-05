530a79c0deab5c0e332ac88173c26a91871118e0
Dr. Németh Tamás <tomuwhu@gmail.com>
2019-07-05 09:13:08 +0200

    .

--- hibas.js
+++ hibas.js
@@ -14,25 +14,33 @@ var app = new Vue({
         next=p==="X"?"O":"X";
         [[1,1],[1,0],[0,1],[-1,1]]
             .forEach( v => {
-                let xp=Number(x), yp=Number(y), maxh=0
-                while (this.arr[yp] && this.arr[yp][xp]===p) 
-                    xp+=v[0], yp+=v[1], maxh++
-                xp=Number(x), yp=Number(y)
-                while (this.arr[yp] && this.arr[yp][xp]===p) 
-                    xp-=v[0], yp-=v[1], maxh++
-                maxh>5 ? this.nyert = `Nyert: <b>${ p }</b>` : null
+                let xp=Number(x), yp=Number(y), maxh=0;
+                while (this.arr[yp] && this.arr[yp][xp]===p) {
+                  xp+=v[0];
+                  yp+=v[1];
+                  maxh++;
+                }
+                xp=Number(x);
+                yp=Number(y);
+                while (this.arr[yp] && this.arr[yp][xp]===p){
+                  xp+=v[0];
+                  yp+=v[1];
+                  maxh++;
+                }
+                console.log(maxh);
+                if ( maxh>5) this.nyert = `Nyert: <b>${ p }</b>`;
             });
     };
     es.onerror = e => {
       this.nyert="Kapcsolat megszakadt!";
       es.close() ;
-    }
+    };
   },
   methods: {
       f(i,j) {
         if (!this.nyert && this.arr[j][i]===" " && rak===0) {
             rak=2;
-            axios.get(base+`${i}-${j}-${next}`)
+            axios.get(base+`${i}-${j}-${next}`);
         }
       }
   }
