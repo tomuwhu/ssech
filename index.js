2446e3371ad943383661d298bde36b514d6c19df
Dr. Németh Tamás <tomuwhu@gmail.com>
2019-07-05 10:51:25 +0200

    .

--- index.js
+++ index.js
@@ -10,28 +10,19 @@ rf  .setenv('frontend');
 
 var server = http.createServer( (req, res) => {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  switch (req.url) {
-    case base: res.end(rf.file()); break;
-    case base+'vue.js': res.end(rf.vuejs); break;
-    case base+'axios.min.js': res.end(rf.axiosjs); break;
-    case base+'axios.min.map': res.end(null); break;
-    case base+'favicon.ico': res.end(null); break;
-    default: {
-        ct.forEach( v => {
-          v.c.send( req.url.slice(1) );
-        });
-        res.end(null);
-    }
+  if (req.url===base) res.end( rf.file() );
+  else {   
+    w = req.url.slice( 1 );
+    if ( w.split('-').length > 2 ) ct.forEach( v =>  v.c.send( w ) );
+    res.end(null);
   }
 });
  
 server.listen(port, myip, () => {
   let sse =new SSE(server);
-  sse.on('connection', c => {
-    ct.push({c, ts: Number( new Date() ) });
-  } );
+  sse.on('connection', c => ct.push({c, ts: Number(new Date()) }) );
 } );
 
-setInterval( () => {
-  ct = ct.filter( v => (Number( new Date() ) - v.ts < 1000000) );
-}, 1000000 );
+setInterval( () =>  
+  ct = ct.filter( v => (Number( new Date() ) - v.ts < 1000000) ), 1000000 
+);
--- static/index.js
+++ static/index.js
@@ -2,37 +2,31 @@ const fs = require('fs');
 
 const rf = {
 
-    hh: `<meta charset='utf-8' /><script src="vue.js"></script><script src="axios.min.js"></script>`,
+    hh: `<meta charset='utf-8' />`,
 
     setenv( s, fn='index' ) {
-        fs.readFile( `./static/vue.js`, (err,v) => {
-            this.vuejs = v.toString() ;
-        });
-        fs.readFile( `./static/axios.min.js`, (err,v) => {
-            this.axiosjs = v.toString() ;
-        });
+        fs.readFile( `./static/vue.js`, (err,v) => this.vuejs = v.toString() );
+        fs.readFile( `./static/axios.min.js`, (err,v) => this.axiosjs = v.toString() );
         fs.readFile( `./${ s }/${ fn }.html`, (err,v) => {
-            if (v)
-                this.html = v.toString() ;
-            else
-                console.error(`No file found: ./${ s }/${ fn }.html`);
+            if (v) this.html = v.toString() ;
+            else console.error(`No file found: ./${ s }/${ fn }.html`);
         });
         fs.readFile( `./${ s }/${ fn }.css`, (err,v) => {
-            if (v)
-                this.css = v.toString() ;
-            else
-                console.error(`No file found: ./${ s }/${ fn }.css`);
+            if (v) this.css = v.toString() ;
+            else console.error(`No file found: ./${ s }/${ fn }.css`);
         });
         fs.readFile( `./${ s }/${ fn }.js`, (err,v) => {
-            if (v)
-                this.js = v.toString() ;
-            else
-                console.error(`No file found: ./${ s }/${ fn }.js\n\n`);
+            if (v) this.js = v.toString() ;
+            else console.error(`No file found: ./${ s }/${ fn }.js\n\n`);
         });
     },
 
     file() {
-        return `${ this.hh }\n${ this.html }\n<style>${ this.css }</style>\n<script>${ this.js }</script>`;
+        return `
+            <script>${ this.axiosjs }</script>
+            ${ this.hh }\n${ this.html }
+            <style>${ this.css }</style>
+            <script>${ this.vuejs }${ this.js }</script>`;
     }
 
 };
