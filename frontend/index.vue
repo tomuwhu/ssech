6dc5cc0d22b5a50d5152eed00469294dabec18ef
Dr. Németh Tamás <tomuwhu@gmail.com>
2019-07-10 08:20:59 +0200

    .

---
+++ frontend/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>Hálózatos táblajátékok - példaprogram</h1>
+        <a href="./amoba/">Amőba</a> - -
+        <a href="./reversi/">Reversi</a><br>
+        <hr>
+        <a href="https://github.com/tomuwhu/ssech" target="_blank">Forrás</a>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data: {
+
+    }
+};
+
+</script>
+
+<style>
+    body{
+    color: #236534;
+    text-shadow: 0 0 1px #111111;
+    text-align:center;
+    }
+    a {
+    font-size: 25px;
+    }
+</style>
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var SSE     = require('sse'),
     app }   = require('singlevue'),
     amoba   = new Svc('amoba'),
     reversi = new Svc('reversi'),
+    main    = new Svc(),
     ct      = [],
     port    = 3004;
 
@@ -11,23 +12,7 @@ app.get( '/amoba/' , (req,res)=> {
 });
 
 app.get( '/' , (req,res)=> {
-  res.send(`
-    <h1>Hálózatos táblajátékok - példaprogram</h1>
-    <a href="./amoba/">Amőba</a> - -
-    <a href="./reversi/">Reversi</a><br>
-    <hr>
-    <a href="https://github.com/tomuwhu/ssech" target="_blank">Forrás</a>
-    <style>
-      body{
-        color: #236534;
-        text-shadow: 0 0 1px #111111;
-        text-align:center;
-      }
-      a {
-        font-size: 25px;
-      }
-    </stye>
-  `) ;
+  res.send(main.vue({})) ;
 });
 
 app.get( '/reversi/' , (req,res)=> {
